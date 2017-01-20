b661e7d32680f34bf4f2fcabdcdbfef322c1816f
Marvin Qian <qianmarv@gmail.com>
2017-01-21 06:55:44 +0800

    --

--- nodejs/Learning_NodeJS/load_albums.js
+++ nodejs/Learning_NodeJS/load_albums.js
@@ -1,17 +1,53 @@
 let [http, fs] = [require('http'), require('fs')];
 
 function load_album_list(fCallback){
+    let root = "albums";
     fs.readdir(
-        "albums",
+        root,
         (err, files)=>{
             if(err){
                 fCallback(err);
                 return;
-            }
-            fCallback(null, files);
+            };
+            /*
+             * Loop won't work
+             *
+             */
+            // aFolders = [];
+            // for(let i; i<files.length; i++){
+            //     file = files[i];
+            //     fs.stat(file, (err, stats)=>{
+            //         if(err){
+            //             fCallback(err);
+            //         }
+            //         if(stats.isDirectory()){
+            //             console.log(file+" is directory."); // no output
+            //             aFolders.push(file);
+            //         }
+            //     });
+            // }
+            // fCallback(err, aFolders);
+            aFolders = [];
+            (function iterator(i){
+                if(i === files.length){
+                    fCallback(err, aFolders);
+                } else {
+                    let file = files[i];
+                    fs.stat(root+"/"+file, (err,stats)=>{
+                        if(err){
+                            fCallback(err);
+                            return;
+                        };
+                        if(stats.isDirectory()){
+                            aFolders.push(file);
+                        };
+                        iterator(i+1);
+                    });
+                };
+            })(0);
         }
     );
-}
+};
 
 function handle_request(req, res){
     console.log("Incoming request: " + req.method + " " + req.url);
