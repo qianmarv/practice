3053462081c863b2e552d81d2a73089e4325f8e2
qianmarv <qianmarv@gmail.com>
2018-06-05 10:48:54 +0800

    Try Flycheck with ABAPInEmacs
    * Way too heave as ABAP Syntac Check Always need to send Request to Background.

---
+++ lisp/flycheck_abap.el
@@ -0,0 +1,46 @@
+(flycheck-define-generic-checker 'abap
+  "A syntax checker for ABAP using abap-mode"
+  :start #'flycheck-abap--start
+  :verify #'flycheck-abap--verify
+  :modes 'abap-mode
+  ;; :error-filter flycheck-abap-error-filter
+  :predicate #'(lambda()
+                 abap-mode))
+
+(defun flycheck-abap-setup()
+  "setup Flycheck abap"
+  (interactive)
+  (add-to-list 'flycheck-checkers 'abap))
+
+(defun flycheck-abap--parse-error (alist checker)
+  (let-alist alist
+    (flycheck-error-new-at .line
+                           .column
+                           (intern .level)
+                           .message)))
+
+(defun flycheck-abap--start (checker callback)
+  (condition-case err
+      (let ((errors (delq nil
+                          (mapcar
+                           (lambda (alist)
+                             (message "%s" alist)
+                             (flycheck-abap--parse-error alist
+                                                         checker))
+                           (abap-check-source t)))))
+        (funcall callback 'finished errors))
+    (error (funcall callback 'errored (error-message-string err)))))
+
+(defun flycheck-abap--verify (_checker)
+  (list
+   (flycheck-verification-result-new
+    :label "ABAP Mode"
+    :message (if abap-mode "enabled" "disabled")
+    :face (if abap-mode 'success '(bold warning)))))
+
+
+(defun test()
+  (interactive)
+  (setq flycheck-mode t)
+
+  (setq flycheck-mode nil))
